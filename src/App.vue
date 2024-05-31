97d605c0dc1b3e77f9a48ad250153d879a5ffbb1
HasanovDoc <hasanov326@mail.ru>
2024-06-01 00:13:58 +0400

    added crime component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
-    <CrimeComponents />
+  <div class="crime_list">
+    <CrimeComponents 
+    v-for="item in dates"
+    :key="item.name"
+    :crimeName="item.name"
+    :articles="item.articles"/>
   </div>
 </template>
 
 <script setup>
 import CrimeComponents from './components/CrimeComponents.vue';
+import {dates} from './data'
 
 </script>
 
@@ -16,6 +21,25 @@ import CrimeComponents from './components/CrimeComponents.vue';
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+}
+
+.crime_list{
+  box-sizing: border-box;
+  width: 100%;
+  min-height: 100vh;
+  background-image: url('./assets/background_crime_2.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  /* margin-left: 200px; */
+  display: flex;
+  flex-direction: column;
+  gap: 6vh;
+  padding: 15vh 10vw;
 }
 </style>
--- src/components/CrimeComponents.vue
+++ src/components/CrimeComponents.vue
@@ -1,9 +1,21 @@
 <template>
-    <div>
-        <button @click="SpolireH1 = !SpolireH1">{{ 'Нажми' }}</button>
-        <v-spoiler v-model="SpolireH1">
-            <div>
-                {{"Преступления против жизни и здоровья"}}
+    <div class="spoiler-container">
+        <div class="spoiler-button_h1" @click="isSpolireH1 = !isSpolireH1">{{ props.crimeName }}</div>
+        <v-spoiler v-model="isSpolireH1">
+            <div class="articles__list">
+                <div class="articles__item"
+                    :class="isSpolireH2[index] ? 'articles__item_active': ''"
+                    v-for="(item, index) in articles"
+                    :key="item.name"
+                    @click="toggleSpoiler(index)"
+                >
+                    <div 
+                        class="spoiler-button_h2" 
+                    >{{ item.name }}</div>
+                    <v-spoiler v-model="isSpolireH2[index]">
+                        <div class="article_result">{{ item.result }}</div>
+                    </v-spoiler>
+                </div>
             </div>
         </v-spoiler>
     </div>
@@ -12,10 +24,108 @@
 <script setup>
 import VSpoiler from 'v-spoiler';
 import 'v-spoiler/dist/v-spoiler.css';
-import { ref } from 'vue';
+import { ref, defineProps } from 'vue';
 
-const SpolireH1 = ref(false)
+const isSpolireH1 = ref(false)
+const isSpolireH2 = ref([])
+
+const props = defineProps({
+    crimeName: String,
+    articles: {
+        name: String,
+        result: String
+    }
+});
+
+const toggleSpoiler = (index) => {
+    if (isSpolireH2.value[index] === undefined) {
+        isSpolireH2.value[index] = true;
+    } else {
+        isSpolireH2.value[index] = !isSpolireH2.value[index];
+    }
+}
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.spoiler-container {
+    max-width: 60vw;
+    display: flex;
+    flex-direction: column;
+}
+
+.spoiler-button_h1 {
+    border-radius: 2px;
+    cursor: pointer;
+    border: 1px solid gray;
+    background-color: rgba(128, 128, 128, 0.532);
+    backdrop-filter: blur(5px);
+    box-shadow: black 0px 0px 5px;
+    width: 52vw;
+    font-size: 2rem;
+    color: white;
+    &:hover{
+        background-color: rgba(128, 128, 128, 0.65);
+    }
+    &:active{
+        box-shadow: black 0px 0px 10px;
+    }
+}
+
+.articles__list {
+    box-sizing: border-box;
+    color: white;
+    margin-top: 10px;
+    max-width: 52vw;
+    text-align: left;
+    padding-left: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    /* padding: 0; */
+}
+
+.articles__item {
+    cursor: pointer;
+    align-items: center;
+    text-align: left;
+    margin: 0 auto;
+    position: relative;
+    background-color: rgba(255, 255, 255, 0.378);
+    backdrop-filter: blur(4px);
+    border-radius: 2px;
+    &:hover{
+        box-shadow: black 0px 0px 5px;
+    }
+    &::before{
+        transition: ease 0.4s;
+        content: '\00BB';
+        position: absolute;
+        left: -2%;
+    }
+}
+
+.articles__item::marker {
+    color: red;
+}
+
+.articles__item_active{
+    &::before{
+        /* content: '\00BB';
+        position: absolute;
+        left: -2%; */
+        transform: rotate(90deg);
+    }
+}
+
+.spoiler-button_h2 {
+    color: black;
+    font-weight: bold;
+}
+
+.article_result{
+    white-space: pre-line;
+    /* margin-top: 10px; */
+    padding: 10px 10px;
+}
+</style>
