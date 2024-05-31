16ec5bffadbaa7f3ab97035d8d222a99692af6fe
HasanovDoc <hasanov326@mail.ru>
2024-06-01 00:16:54 +0400

    build | init

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import VSpoiler from 'v-spoiler';
-import 'v-spoiler/dist/v-spoiler.css';
-
-const app = createApp(App);
-app.component('VSpoiler', VSpoiler);
-app.mount('#app');
